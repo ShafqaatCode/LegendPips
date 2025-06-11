1e520ac78b493bd17a6b51a696a99c4014e581c7
ShafqaatCode <rajashajaatali@gmail.com>
2025-06-12 00:16:10 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-
-import './App.css'
-import styled from 'styled-components'
-
-
-const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.background};
-  color: ${({ theme }) => theme.colors.text};
-  font-family: ${({ theme }) => theme.font.family};
-  padding: 2rem;
-  text-align: center;
-`;
-
+import "./App.css";
 
 function App() {
-  
-
-  return (
-    <>
-      <Wrapper>
-       LegendPips
-      </Wrapper>
-    </>
-  )
+  return <>LegendPips</>;
 }
 
-export default App
+export default App;
