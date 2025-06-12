570871f5a51f80866d5bb96c111c5bdce9cc50fe
ShafqaatCode <rajashajaatali@gmail.com>
2025-06-12 07:08:47 +0500

    font Poppins is Added

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,4 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
 
 export const GlobalStyles = createGlobalStyle`
   *, *::before, *::after {
