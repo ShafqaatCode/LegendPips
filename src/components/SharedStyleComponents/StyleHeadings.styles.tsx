a4127f5702c9a61ed4e0595a21ccd8601de501c3
ShafqaatCode <rajashajaatali@gmail.com>
2025-06-14 08:35:42 +0500

    minor changes 005

--- src/components/SharedStyleComponents/StyleHeadings.styles.tsx
+++ src/components/SharedStyleComponents/StyleHeadings.styles.tsx
@@ -5,14 +5,13 @@ export const SectionHeading = styled.h1`
   font-family: 'Poppins', sans-serif;
   font-weight: 700;
   font-size: 64px;
-  line-height: 120px;
+  line-height: 100px;
   letter-spacing: -0.6%;
   text-align: center;
   text-transform: uppercase;
+  `;
   
-`;
-
-export const UpperHeading = styled.h2`
+  export const UpperHeading = styled.h2`
   font-family: 'Poppins', sans-serif;
   font-weight: 600;
   font-size: 20px;
@@ -34,6 +33,7 @@ export const SubHeading = styled.h2`
   width:850px;
   margin:auto;
   // border: 2px solid red;
+  margin-bottom: 2rem;
    
 `;
 
--- src/components/Signals/Signals.tsx
+++ src/components/Signals/Signals.tsx
@@ -6,15 +6,16 @@ import {
   SubHeading,
 } from "../SharedStyleComponents/StyleHeadings.styles";
 import SignalBoxImg from "../../assets/Frame 157.svg";
+import MathImg from "../../assets/icons/all-svgrepo-com 1.svg"
 
 const SignalsSection = styled.section`
-    display:flex;
-    align-items:center;
-    justify-content: center;
-    flex-direction: column;
-    height: 700px;
-    border: 2px solid red;
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-height: 700px;
+  border: 2px solid red;
+  margin:1rem;
 `;
 
 const SignalImgContainer = styled.div`
@@ -22,13 +23,28 @@ const SignalImgContainer = styled.div`
   align-items: center;
   justify-content: center;
   gap: 2rem;
+  flex-wrap: wrap;
+
+  img {
+    height: 250px;
+  }
+`;
+
+const UpperHeadingDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.4rem;
 `;
 
 const Signals: React.FC = () => {
   return (
     <SignalsSection>
       <div>
-        <UpperHeading>All IN ONE TRADING PlATEFORM</UpperHeading>
+        <UpperHeadingDiv>
+            <img src={MathImg} alt="" />
+          <UpperHeading>All IN ONE TRADING PlATEFORM</UpperHeading>
+        </UpperHeadingDiv>
         <SectionHeading>LIVE SIGNALS</SectionHeading>
         <SubHeading>
           Trade, earn, repeat. With rebates, expert tools, and a strong
