4adae8e4dd9d89555a61a00955c0be8ec1051225
ShafqaatCode <rajashajaatali@gmail.com>
2025-07-15 17:25:29 +0500

    implement contest page

--- src/pages/Contests/Contests.tsx
+++ src/pages/Contests/Contests.tsx
@@ -1,41 +1,114 @@
 import React from 'react'
-import Banner from '../../components/Banner/Banner'
-import bannerImg from "../../assets/banner/BannerBg.jpg";
-import BannerHeadingSet from '../../components/SharedComponents/BannerHeadingSet';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { FaUpload } from 'react-icons/fa';
 
-const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.primary};
-  height: 600px;
- 
-  margin-top: 60px;
+
+const MainContainer = styled.main`
+
+margin-top: 135px;
+  height: 700px;
+  border: 2px solid red;
+  margin:135px auto;
+@media (max-width: ${({ theme }) => theme.breakpoints.laptop})
+{
+  margin-top: 80px;
+} 
+  
+`
+
+const ToplinksBar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: auto;
+  padding: 10px;
+
+`
+
+const FeedbackLinks = styled.div`
+  
+
+`
+const BroadCrumbRow = styled.div`
+  
+
+`
+
+const AdvertisementBar = styled.div`
+  width: 80%;
+  margin: auto;
+  height: 100px;
+  border: 2px solid red;
+  text-align: center;
   display: flex;
   align-items: center;
   justify-content: center;
 `
-const Overlay = styled.div`
-  position: absolute;
-  inset: 0;
-  background: linear-gradient(
-    to right,
-    rgba(0, 66, 153, 0.6),
-    rgba(0, 66, 153, 0.5),
-    rgba(0, 66, 153, 0.6)
-  );
-  z-index: 1;
-`;
+
+const ContestsSection = styled.section`
+background-color: ${({ theme }) => theme.colors.primary};
+display: grid;
+grid-template-columns: repeat(10, 1fr);
+color: white;
+align-items: center;
+font-size: 16px;
+  
+  div{
+    border: 2px solid green;
+    text-align: center;
+  }
+`
 const Contests: React.FC = () => {
   return (
+    <MainContainer>
+      <ToplinksBar>
+        <BroadCrumbRow>
+          <Link to="/home">
+            Home
+          </Link>
+          <span> / </span>
 
-    <Wrapper>
-      <Overlay />
-      <BannerHeadingSet subText='...' mainHeading='.......' upperText='...' />
-    </Wrapper>
+          Contests
+
+        </BroadCrumbRow>
+        <FeedbackLinks>
+          <Link to="/Feedback">
+            Feedback
+          </Link>
+          <span> | </span>
+          <Link to="/Feedback">
+            Help Improve this page
+          </Link>
+          <span> | </span>
+
+          <Link to="/share">
+            Share <FaUpload />
+          </Link>
 
 
+        </FeedbackLinks>
+      </ToplinksBar>
+      <AdvertisementBar>
+        Advertisement
+      </AdvertisementBar>
 
+      <ContestsSection>
+        <div>Contest Description</div>
+        <div>Requirements</div>
+        <div>Prize</div>
+        <div>Contestents</div>
+        <div>Start Date</div>
+        <div>End Date</div>
+        <div>Post</div>
+        <div>Registration Ends</div>
+        <div>Status</div>
+        <div>Registration</div>
+        
 
+      </ContestsSection>
 
+    </MainContainer>
   )
 }
 
